459214cc410ea823d423dc6fd5b8f9a438fd0cbd
kumarisnehasaw <kumarisnehasaw@gmail.com>
2025-10-31 13:17:22 +0530

    intial

--- server/server.js
+++ server/server.js
@@ -77,3 +77,4 @@ app.post("/login", async (req, res) => {
 /* ---------------------- SERVER START ---------------------- */
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+//hehehe
